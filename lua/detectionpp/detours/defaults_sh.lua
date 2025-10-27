9c13bf0cc59f0b974c2c16da1a5dc39fa58000e5
march <106459595+marchc1@users.noreply.github.com>
2025-10-27 15:54:44 -0700

    Experimental chip per-basis function detouring

---
+++ lua/detectionpp/detours/defaults_sh.lua
@@ -0,0 +1,6 @@
+function DetectionPP.DefaultVector() return Vector(0, 0, 0) end
+function DetectionPP.DefaultAngle() return Angle(0, 0, 0) end
+function DetectionPP.DefaultVectorAngle() return Vector(0, 0, 0), Angle(0, 0, 0) end
+function DetectionPP.DefaultEntity() return NULL end
+function DetectionPP.DefaultNumber() return 0 end
+function DetectionPP.DefaultString() return "" end
--- lua/detectionpp/detours/expression2_sv.lua
+++ lua/detectionpp/detours/expression2_sv.lua
@@ -1,19 +1,46 @@
 local Detours = DetectionPP.Detours
 
-timer.Simple(0.2, function()
-    local OriginalCompile; OriginalCompile = Detours.New("E2Lib.Compiler.CompileExpr", function(...)
-        local op, ty = OriginalCompile(...)
-        if ty == "e" then
-            local oldOp = op
-            op = function(ctx, ...)
-                local ret = oldOp(ctx, ...)
-                if DetectionPP.CantDetect(ret, ctx.player) then
-                    return NULL
-                end
-                return ret
-            end
+local function DetourE(E2Sig, Default)
+    local Func Func = Detours.Expression2(E2Sig, function(Scope, Args, ...)
+        if DetectionPP.CantDetect(Args[1], Scope.player) then
+            return Default()
         end
-
-        return op, ty
+        return Func(Scope, Args, ...)
     end)
-end)
+end
+
+local function DetourB(E2Sig, Default)
+    local Func Func = Detours.Expression2(E2Sig, function(Scope, Args, ...)
+        if IsValid(Args[1]) and DetectionPP.CantDetect(Args[1]:GetEntity(), Scope.player) then
+            return Default()
+        end
+        return Func(Scope, Args, ...)
+    end)
+end
+
+DetourE("e:pos()", DetectionPP.DefaultVector)
+DetourB("b:pos()", DetectionPP.DefaultVector)
+
+DetourE("e:aimPos()", DetectionPP.DefaultVector)
+DetourE("e:attachmentPos(n)", DetectionPP.DefaultVector)
+DetourE("e:attachmentPos(s)", DetectionPP.DefaultVector)
+DetourE("e:shootPos()", DetectionPP.DefaultVector)
+
+DetourE("e:toWorld(a)", DetectionPP.DefaultAngle)
+DetourE("e:toWorld(v)", DetectionPP.DefaultVector)
+DetourE("e:toLocal(a)", DetectionPP.DefaultAngle)
+DetourE("e:toLocal(v)", DetectionPP.DefaultVector)
+DetourE("e:toWorldAxis(v)", DetectionPP.DefaultVector)
+DetourE("e:toLocalAxis(v)", DetectionPP.DefaultVector)
+
+DetourB("b:toWorld(v)", DetectionPP.DefaultVector)
+DetourB("b:toLocal(v)", DetectionPP.DefaultVector)
+
+DetourE("e:angVel()", DetectionPP.DefaultAngle)
+DetourE("e:angVelVector()", DetectionPP.DefaultVector)
+DetourE("e:angles()", DetectionPP.DefaultAngle)
+DetourE("e:attachmentAng(n)", DetectionPP.DefaultAngle)
+DetourE("e:attachmentAng(s)", DetectionPP.DefaultAngle)
+DetourE("e:eyeAngles()", DetectionPP.DefaultAngle)
+DetourE("e:eyeAnglesVehicle()", DetectionPP.DefaultAngle)
+DetourE("e:ragdollGetAng()", DetectionPP.DefaultAngle)
