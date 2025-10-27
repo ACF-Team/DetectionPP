17aeacc2d89f3e3f1aa7ef0e5e40d5fcafe6ff64
march <106459595+marchc1@users.noreply.github.com>
2025-10-26 19:52:30 -0700

    wrong order..

--- lua/detectionpp/core/permissions_sv.lua
+++ lua/detectionpp/core/permissions_sv.lua
@@ -159,7 +159,7 @@ net.Receive("DetectionPP_Friends", function(_, Player)
     local SteamID = Player:SteamID64()
     local TimeRemaining = TryAllocTimeout(ALLOC_TABLE_SET, SteamID)
     if TimeRemaining > 0 then
-        DetectionPP.Notify(Player, NOTIFY_GENERIC, string.format("Rate limited; cannot set friends (try again in %.2f seconds)", TimeRemaining))
+        DetectionPP.Notify(Player, string.format("Rate limited; cannot set friends (try again in %.2f seconds)", NOTIFY_GENERIC, TimeRemaining))
         return
     end
 
